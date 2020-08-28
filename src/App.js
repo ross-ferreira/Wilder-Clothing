ae9d2195a356175b4fede2771b2798f7c4afaa23
Ross Ferreira <ross.ferreira@sd-lt-os-20902.home>
2020-08-28 08:10:11 +0100

    broken homepage  into smaller pieces, finished styling menu-items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Hompage.component';
+import Homepage from './pages/homepage/Hompage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/Hompage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/directory/Directory.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
