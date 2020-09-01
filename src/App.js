2be2465ec200164c1cd2fe095fe7b160749dee7f
Ross Ferreira <ross.ferreira@sd-lt-os-20902.home>
2020-09-01 07:11:03 +0100

    update menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Hompage.component';
+
+const HatsPage = () => {};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
