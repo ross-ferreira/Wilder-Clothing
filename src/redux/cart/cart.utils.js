bda5e82cde5e492928b374872acb7534d72b8aa4
Ross Ferreira <ross.ferreira@sd-lt-os-20902.home>
2020-09-15 07:20:53 +0100

    Updated cart utils for cart reducer to add items to cart

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+//Checker to see if item has already been added to cart
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : { cartItem }
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
