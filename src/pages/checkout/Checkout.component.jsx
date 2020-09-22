4a6fe9eaa1d4a070240c71d09dd73f78d298acb9
Ross Ferreira <ross.ferreira@superdry.com>
2020-09-22 07:03:48 +0100

    added stripe payments

--- src/pages/checkout/Checkout.component.jsx
+++ src/pages/checkout/Checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/Checkout-Item/Checkout-Item.component';
+import StripeCheckoutButton from '../../components/Stripe-Button/Stripe-Button.component';
 
 import {
   selectCartItems,
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      **Please use the following test credit card for payments
+      <br />
+      4242 4242 4242 4242 - EXP:12/20 -CVV:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
